f7cad83e12d601163fbc73ddd01867e1f8364f03
Carlos Barrenechea <peztoa@gmail.com>
2016-11-23 22:08:40 -0500

    Order Activity

---
+++ SeguimientoCamiones/app/src/main/java/como/isil/mynotes/rest/OrderActivity.java
@@ -0,0 +1,13 @@
+package como.isil.mynotes.rest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.isil.mynotes.rest.R;
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
