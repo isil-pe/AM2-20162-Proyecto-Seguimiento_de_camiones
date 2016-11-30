60fb40895e14c0970afd9492b940c9958df732f5
Carlos Barrenechea <peztoa@gmail.com>
2016-11-30 03:30:30 -0500

    Marcadores funcionando en el mapa

--- SeguimientoCamiones/app/src/main/java/como/isil/mynotes/rest/OrderActivity.java
+++ SeguimientoCamiones/app/src/main/java/como/isil/mynotes/rest/OrderActivity.java
@@ -1,27 +1,22 @@
 package como.isil.mynotes.rest;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
-import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.isil.mynotes.rest.R;
@@ -29,10 +24,8 @@ import com.isil.mynotes.rest.R;
 import como.isil.mynotes.rest.entity.OrderEntity;
 import como.isil.mynotes.rest.utils.LocationService;
 import como.isil.mynotes.rest.view.fragments.OrderDetailFragment;
-import como.isil.mynotes.rest.view.fragments.TrackMap;
-import como.isil.mynotes.rest.view.listeners.OnOrderListener;
 
-public class OrderActivity extends AppCompatActivity implements View.OnClickListener{
+public class OrderActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback{
 
     public static final int DETAIL_ORDER = 101;
     private static final String TAG = "OrderActivity";
@@ -63,39 +56,111 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
         id_orden = getIntent().getStringExtra("id_orden");
         initializeUI();
 
+        mapElements();
+
         btnStart= findViewById(R.id.btnStart);
         btnStop= findViewById(R.id.btnStop);
 
         btnStart.setOnClickListener(this);
+
+
     }
+
+
 
     private void initializeUI() {
         try {
-            initilizeMap();
+            initializeMap();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void initilizeMap() {
+    private void initializeMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapita);
+        mapFragment.getMapAsync(this);
+
+        /*
+
         if (googleMap == null) {
-            mCustomMapFragment = ((CustomMapFragment) getFragmentManager().findFragmentById(R.id.map));
+            mCustomMapFragment = ((CustomMapFragment) getFragmentManager().findFragmentById(R.id.mapita));
             mCustomMapFragment.setOnDragListener((MapWrapperLayout.OnDragListener) getApplicationContext());
-            googleMap = mCustomMapFragment.getMap();
-            locMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            Location lastLoc = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            final double lat = lastLoc.getLatitude();
-            final double lng = lastLoc.getLongitude();
-            final LatLng lasLatLng = new LatLng(lat, lng);
-            googleMap.setMyLocationEnabled(true);
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lasLatLng, 18.5F));
-            if (googleMap == null) {
-                Toast.makeText(getApplicationContext(),
-                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
+            mCustomMapFragment.getMapAsync(this);
+
+        }*/
     }
+
+
+    public void onMapReady(GoogleMap googleMap) {
+
+        locMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(OrderActivity.this, "Por favor ceder permisos en configuración.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        Location lastLoc = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        final double lat = lastLoc.getLatitude();
+        final double lng = lastLoc.getLongitude();
+
+        TextView txvCoord = (TextView) findViewById(R.id.tviCoord);
+        txvCoord.setText("Latitud: "+lat+", Longitud"+lng);
+
+
+        final LatLng lasLatLng = new LatLng(lat, lng);
+        Toast.makeText(getApplicationContext(),
+                "Latitud: "+lat+", Longitud"+lng, Toast.LENGTH_SHORT)
+                .show();
+        googleMap.setMyLocationEnabled(true);
+        if (googleMap == null) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(Double.parseDouble(origen_latitud), Double.parseDouble(origen_longitud)),12));
+
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(Double.parseDouble(origen_latitud), Double.parseDouble(origen_longitud)))
+                .title("Origen"));
+    }
+
+
+
+    private void mapElements(){
+        locMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(OrderActivity.this, "Por favor ceder permisos en configuración.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        Location lastLoc = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        final double lat = lastLoc.getLatitude();
+        final double lng = lastLoc.getLongitude();
+
+        TextView txvCoord = (TextView) findViewById(R.id.tviCoord);
+        txvCoord.setText("Latitud: "+lat+", Longitud"+lng);
+
+
+        final LatLng lasLatLng = new LatLng(lat, lng);
+        Toast.makeText(getApplicationContext(),
+                "Latitud: "+lat+", Longitud"+lng, Toast.LENGTH_SHORT)
+                .show();
+/*
+        if (googleMap == null) {
+            googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(Double.parseDouble(origen_latitud), Double.parseDouble(origen_longitud)))
+                    .title("Origen"));
+        }*/
+    }
+
 
     @Override
     public void onClick(View v) {
